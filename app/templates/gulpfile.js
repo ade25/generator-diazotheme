066f959f5b856a6d8de36e20d79254e71a74ab20
Christoph Böhner-Figas <boehner@team23.de>
2016-09-05 17:50:26 +0200

    Update gulp tasks and diazo rule template
    Add favicon generation to gulp file and adopt the approach in the
    diazo rule file. Update the rules for portlets and umd js loading

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -26,19 +26,21 @@ var basePaths = {
 
 var sourcesJS = {
     base: [
-        basePaths.src + 'bootstrap-without-jquery/bootstrap3/bootstrap-without-jquery.js',
-        basePaths.src + 'lazysizes/lazysizes.js',
-        basePaths.src + 'flickity/dist/flickity.pkgd.js'
+        basePaths.app + 'scripts/app.js'
     ],
     all: [
-        basePaths.src + 'jquery/dist/jquery.js',
-        basePaths.src + 'modernizr/modernizr.js',
-        basePaths.src + 'bootstrap-without-jquery/bootstrap3/bootstrap-without-jquery.js',
-        basePaths.src + 'mailcheck/src/mailcheck.js',
-        basePaths.src + 'JVFloat/jvfloat.js',
-        basePaths.src + 'hideShowPassword/hideShowPassword.js',
-        basePaths.src + 'lazysizes/lazysizes.js',
-        basePaths.src + 'flickity/dist/flickity.pkgd.js'
+        basePaths.src + '/jquery/dist/jquery.js'
+        basePaths.src + '/modernizr/modernizr.js',
+        basePaths.src + '/tether/dist/js/tether.min.js',
+        basePaths.src + '/bootstrap/dist/js/bootstrap.js',
+        basePaths.src + '/mailcheck/src/mailcheck.js',
+        basePaths.src + '/JVFloat/jvfloat.js',
+        basePaths.src + '/hideShowPassword/hideShowPassword.js',
+        basePaths.src + '/lazysizes/plugins/ls.parent-fit.js',
+        basePaths.src + '/lazysizes/lazysizes.js',
+        basePaths.src + '/respimage/respimage.js',
+        basePaths.src + '/flickity/dist/flickity.pkgd.js',
+        basePaths.app + '/scripts/main.js'
 
     ]
 };
@@ -172,6 +174,102 @@ gulp.task('replace', () => {
             preserveOrder: true
         }))
         .pipe(gulp.dest(basePaths.dev))
+});
+
+
+// Generate the icons. This task takes a few seconds to complete.
+// You should run it at least once to create the icons. Then,
+// you should run it whenever RealFaviconGenerator updates its
+// package (see the check-for-favicon-update task below).
+gulp.task('generate-favicon', function(done) {
+    $.realFavicon.generateFavicon({
+        masterPicture: basePaths.app + '/assets/ico/favicon.png',
+        dest: basePaths.dist + '/assets/ico/',
+        iconsPath: '/',
+        design: {
+            ios: {
+                pictureAspect: 'backgroundAndMargin',
+                backgroundColor: '#ffffff',
+                margin: '14%',
+                assets: {
+                    ios6AndPriorIcons: false,
+                    ios7AndLaterIcons: false,
+                    precomposedIcons: false,
+                    declareOnlyDefaultIcon: true
+                }
+            },
+            desktopBrowser: {},
+            windows: {
+                pictureAspect: 'noChange',
+                backgroundColor: '#2d89ef',
+                onConflict: 'override',
+                assets: {
+                    windows80Ie10Tile: false,
+                    windows10Ie11EdgeTiles: {
+                        small: false,
+                        medium: true,
+                        big: false,
+                        rectangle: false
+                    }
+                }
+            },
+            androidChrome: {
+                pictureAspect: 'backgroundAndMargin',
+                margin: '17%',
+                backgroundColor: '#ffffff',
+                themeColor: '#ffffff',
+                manifest: {
+                    name: pkg.name,
+                    display: 'standalone',
+                    orientation: 'notSet',
+                    onConflict: 'override',
+                    declared: true
+                },
+                assets: {
+                    legacyIcon: false,
+                    lowResolutionIcons: false
+                }
+            },
+            safariPinnedTab: {
+                pictureAspect: 'silhouette',
+                themeColor: '#5bbad5'
+            }
+        },
+        settings: {
+            compression: 3,
+            scalingAlgorithm: 'Mitchell',
+            errorOnImageTooSmall: false
+        },
+        versioning: {
+            paramName: 'v',
+            paramValue: 'yyxQeA5p49'
+        },
+        markupFile: FAVICON_DATA_FILE
+    }, function() {
+        done();
+    });
+});
+
+// Inject the favicon markups in your HTML pages. You should run
+// this task whenever you modify a page. You can keep this task
+// as is or refactor your existing HTML pipeline.
+gulp.task('inject-favicon-markups', function() {
+    gulp.src([ basePaths.app + '_includes/base/head.html' ])
+        .pipe($.realFavicon.injectFaviconMarkups(JSON.parse(fs.readFileSync(FAVICON_DATA_FILE)).favicon.html_code))
+        .pipe(gulp.dest(basePaths.app + '_includes/base/'));
+});
+
+// Check for updates on RealFaviconGenerator (think: Apple has just
+// released a new Touch icon along with the latest version of iOS).
+// Run this task from time to time. Ideally, make it part of your
+// continuous integration system.
+gulp.task('check-for-favicon-update', function(done) {
+    var currentVersion = JSON.parse(fs.readFileSync(FAVICON_DATA_FILE)).version;
+    $.realFavicon.checkForUpdates(currentVersion, function(err) {
+        if (err) {
+            throw err;
+        }
+    });
 });
 
 gulp.task('clean', del.bind(null, ['.tmp', basePaths.dist]));
