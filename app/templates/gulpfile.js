e1adf438f7e9c63eb7825ee84929f70b99d548b0
Christoph Böhner-Figas <boehner@team23.de>
2016-10-24 12:21:44 +0200

    Update package configuration

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -3,6 +3,7 @@ import gulp from 'gulp';
 import gulpLoadPlugins from 'gulp-load-plugins';
 import {create as bsCreate} from 'browser-sync';
 import del from 'del';
+import revDel from 'rev-del';
 import args from 'yargs';
 import {stream as wiredep} from 'wiredep';
 
@@ -15,35 +16,27 @@ var cfg = require('./config.json');
 var fs = require('fs');
 
 // File where the favicon markups are stored
-var FAVICON_DATA_FILE = 'faviconData.json';
+var FAVICON_DATA_FILE = cfg.favicon.dataFile;
 
 var messages = {
     jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
 };
 
-var basePaths = {
-    app: 'app',
-    dev: '_site',
-    dist: 'dist',
-    diazoPrefix: '/++theme++pkg.name.sitetheme',
-    src: 'node_modules/'
-};
-
 var sourcesJS = {
     base: [
-        basePaths.src + 'bootstrap-without-jquery/bootstrap3/bootstrap-without-jquery.js',
-        basePaths.src + 'lazysizes/lazysizes.js',
-        basePaths.src + 'flickity/dist/flickity.pkgd.js'
+        cfg.paths.src + 'bootstrap-without-jquery/bootstrap3/bootstrap-without-jquery.js',
+        cfg.paths.src + 'lazysizes/lazysizes.js',
+        cfg.paths.src + 'flickity/dist/flickity.pkgd.js'
     ],
     all: [
-        basePaths.src + 'jquery/dist/jquery.js',
-        basePaths.src + 'modernizr/modernizr.js',
-        basePaths.src + 'bootstrap-without-jquery/bootstrap3/bootstrap-without-jquery.js',
-        basePaths.src + 'mailcheck/src/mailcheck.js',
-        basePaths.src + 'JVFloat/jvfloat.js',
-        basePaths.src + 'hideShowPassword/hideShowPassword.js',
-        basePaths.src + 'lazysizes/lazysizes.js',
-        basePaths.src + 'flickity/dist/flickity.pkgd.js'
+        cfg.paths.src + 'jquery/dist/jquery.js',
+        cfg.paths.src + 'modernizr/modernizr.js',
+        cfg.paths.src + 'bootstrap-without-jquery/bootstrap3/bootstrap-without-jquery.js',
+        cfg.paths.src + 'mailcheck/src/mailcheck.js',
+        cfg.paths.src + 'JVFloat/jvfloat.js',
+        cfg.paths.src + 'hideShowPassword/hideShowPassword.js',
+        cfg.paths.src + 'lazysizes/lazysizes.js',
+        cfg.paths.src + 'flickity/dist/flickity.pkgd.js'
 
     ]
 };
@@ -64,11 +57,11 @@ gulp.task('browser-sync', function () {
         notify: false,
         port: 9499,
         server: {
-            baseDir: ['.tmp', basePaths.dist],
+            baseDir: ['.tmp', cfg.paths.dist],
             routes: {
-                '/scripts': basePaths.dist + '/scripts',
-                '/styles': basePaths.dist + '/styles',
-                '/assets': basePaths.dist + '/assets',
+                '/scripts': cfg.paths.dist + 'scripts',
+                '/styles': cfg.paths.dist + 'styles',
+                '/assets': cfg.paths.dist + 'assets',
             }
         }
     });
@@ -79,24 +72,24 @@ gulp.task('bs-reload', function () {
 });
 
 gulp.task('styles', () => {
-    return gulp.src(basePaths.app + '/sass/main.scss')
+    return gulp.src(cfg.paths.app + 'sass/main.scss')
         .pipe($.plumber())
         .pipe($.sourcemaps.init())
         .pipe($.sass.sync({
             outputStyle: 'expanded',
             precision: 10,
-            includePaths: [basePaths.src]
+            includePaths: [cfg.paths.src]
         }).on('error', $.sass.logError))
         .pipe($.autoprefixer({browsers: ['last 1 version']}))
         //.pipe($.csscomb())
-        .pipe(gulp.dest(basePaths.dist + '/styles/'))
+        .pipe(gulp.dest(cfg.paths.dist + 'styles/'))
         .pipe($.cssnano())
         .pipe($.rename({
             basename: pkg.name,
             suffix: '.min'
         }))
         .pipe($.sourcemaps.write())
-        .pipe(gulp.dest(basePaths.dist + '/styles/'))
+        .pipe(gulp.dest(cfg.paths.dist + 'styles/'))
         .pipe(browserSync.reload({stream: true}))
 });
 
@@ -111,68 +104,68 @@ gulp.task('scripts', () => {
         // .pipe($.jshint())
         // .pipe($.jshint.reporter('default'))
         .pipe($.concat(pkg.name + '.js'))
-        .pipe(gulp.dest(basePaths.dist + '/scripts/'))
+        .pipe(gulp.dest(cfg.paths.dist + 'scripts/'))
         .pipe($.rename({suffix: '.min'}))
         .pipe($.uglify())
-        .pipe(gulp.dest(basePaths.dist + '/scripts/'))
+        .pipe(gulp.dest(cfg.paths.dist + 'scripts/'))
         .pipe(browserSync.reload({stream: true}));
 })
 ;
 
 gulp.task('images', () => {
-    return gulp.src(basePaths.app + '/assets/img/**/*')
+    return gulp.src(cfg.paths.app + 'assets/img/**/*')
         .pipe($.if($.if.isFile, $.cache($.imagemin({
-                progressive: true,
-                interlaced: true,
-                // don't remove IDs from SVGs, they are often used
-                // as hooks for embedding and styling
-                svgoPlugins: [{cleanupIDs: false}]
-            }))
+            progressive: true,
+            interlaced: true,
+            // don't remove IDs from SVGs, they are often used
+            // as hooks for embedding and styling
+            svgoPlugins: [{cleanupIDs: false}]
+        }))
             .on('error', function (err) {
                 console.log(err);
                 this.end();
             })))
-        .pipe(gulp.dest(basePaths.dist + '/assets/img'));
+        .pipe(gulp.dest(cfg.paths.dist + 'assets/img'));
 })
 ;
 
 gulp.task('fonts', () => {
     return gulp.src(require('main-bower-files')({
-            filter: '**/*.{eot,svg,ttf,woff,woff2}'
-        }).concat(basePaths.app + '/assets/fonts/**/*'))
+        filter: '**/*.{eot,svg,ttf,woff,woff2}'
+    }).concat(cfg.paths.app + 'assets/fonts/**/*'))
         .pipe(gulp.dest('.tmp/fonts'))
-        .pipe(gulp.dest(basePaths.dist + '/assets/fonts'));
+        .pipe(gulp.dest(cfg.paths.dist + 'assets/fonts'));
 })
 ;
 
 gulp.task('html', () => {
-    return gulp.src(basePaths.dev + '{,*/}*.html')
+    return gulp.src(cfg.paths.dev + '{,*/}*.html')
         .pipe($.minifyHtml())
-        .pipe(gulp.dest(basePaths.dist));
+        .pipe(gulp.dest(cfg.paths.dist));
 })
 ;
 
 gulp.task('cb', () => {
-    return gulp.src(basePaths.dist + 'styles/*.min.css')
+    return gulp.src(cfg.paths.dist + 'styles/*.min.css')
         .pipe($.rev())
-        .pipe(gulp.dest(basePaths.dist + '/styles'))
+        .pipe(gulp.dest(cfg.paths.dist + 'styles'))
         .pipe($.rev.manifest())
-        .pipe($.revDel({dest: basePaths.dist + '/styles'}))
-        .pipe(gulp.dest(basePaths.dist + '/styles'))
+        .pipe(revDel({dest: cfg.paths.dist + 'styles'}))
+        .pipe(gulp.dest(cfg.paths.dist + 'styles'))
 }
 )
 ;
 
 gulp.task('revreplace', () => {
-    var manifest = gulp.src(basePaths.dist + '/styles/rev-manifest.json');
-return gulp.src(basePaths.dev + '/{,*/}*.html')
+    var manifest = gulp.src(cfg.paths.dist + '/styles/rev-manifest.json');
+return gulp.src(cfg.paths.dev + '/{,*/}*.html')
     .pipe(revReplace({manifest: manifest}))
-    .pipe(gulp.dest(basePaths.dev));
+    .pipe(gulp.dest(cfg.paths.dev));
 })
 ;
 
 gulp.task('replace', () => {
-    return gulp.src(basePaths.dev + '/{,*/}*.html')
+    return gulp.src(cfg.paths.dev + '/{,*/}*.html')
         .pipe(replace({
             patterns: [
                 {
@@ -183,40 +176,40 @@ gulp.task('replace', () => {
             usePrefix: false,
             preserveOrder: true
         }))
-        .pipe(gulp.dest(basePaths.dev))
+        .pipe(gulp.dest(cfg.paths.dev))
 });
 
-gulp.task('clean', del.bind(null, ['.tmp', basePaths.dist]));
+gulp.task('clean', del.bind(null, ['.tmp', cfg.paths.dist]));
 
 gulp.task('serve', ['styles', 'scripts', 'jekyll-build', 'html'], () => {
-  browserSync.init({
+    browserSync.init({
     notify: false,
     port: 9499,
     server: {
-      baseDir: ['.tmp', basePaths.dist],
-      routes: {
-        '/scripts': basePaths.dist + '/scripts',
-        '/styles': basePaths.dist + '/styles',
-        '/assets': basePaths.dist + '/assets',
-      }
+        baseDir: ['.tmp', cfg.paths.dist],
+        routes: {
+            '/scripts': cfg.paths.dist + '/scripts',
+            '/styles': cfg.paths.dist + '/styles',
+            '/assets': cfg.paths.dist + '/assets',
+        }
     }
-  });
+});
 
-  gulp.watch([
-    basePaths.app + '/*.html',
-    basePaths.app + '/scripts/*.js',
-    basePaths.app + '/styles/*.css',
-  ]).on('change', browserSync.reload);
+gulp.watch([
+    cfg.paths.app + '/*.html',
+    cfg.paths.app + '/scripts/*.js',
+    cfg.paths.app + '/styles/*.css',
+]).on('change', browserSync.reload);
 
-  gulp.watch(basePaths.app + "sass/**/*.scss", ['styles']);
-  gulp.watch(basePaths.app + "scripts/**/*.js", ['scripts']);
-  gulp.watch(basePaths.app + "{,*/}*.html", ['jekyll-build', 'html']);
+gulp.watch(cfg.paths.app + "sass/**/*.scss", ['styles']);
+gulp.watch(cfg.paths.app + "scripts/**/*.js", ['scripts']);
+gulp.watch(cfg.paths.app + "{,*/}*.html", ['jekyll-build', 'html']);
 });
 
 gulp.task('default', ['browser-sync'], function () {
-    gulp.watch(basePaths.app + "sass/**/*.scss", ['styles']);
-    gulp.watch(basePaths.app + "scripts/**/*.js", ['scripts']);
-    gulp.watch(basePaths.app + "*.html", ['bs-reload']);
+    gulp.watch(cfg.paths.app + "sass/**/*.scss", ['styles']);
+    gulp.watch(cfg.paths.app + "scripts/**/*.js", ['scripts']);
+    gulp.watch(cfg.paths.app + "*.html", ['bs-reload']);
 });
 
 // Generate the icons. This task takes a few seconds to complete.
@@ -225,13 +218,13 @@ gulp.task('default', ['browser-sync'], function () {
 // package (see the check-for-favicon-update task below).
 gulp.task('generate-favicon', function(done) {
     $.realFavicon.generateFavicon({
-        masterPicture: basePaths.app + '/assets/ico/favicon.png',
-        dest: basePaths.dist + '/assets/ico/',
+        masterPicture: cfg.paths.app + cfg.favicon.iconPath + cfg.favicon.masterPicture,
+        dest: cfg.paths.dist + cfg.favicon.iconPath,
         iconsPath: '/',
         design: {
             ios: {
                 pictureAspect: 'backgroundAndMargin',
-                backgroundColor: '#ffffff',
+                backgroundColor: cfg.favicon.ios,
                 margin: '14%',
                 assets: {
                     ios6AndPriorIcons: false,
@@ -243,7 +236,7 @@ gulp.task('generate-favicon', function(done) {
             desktopBrowser: {},
             windows: {
                 pictureAspect: 'noChange',
-                backgroundColor: '#2d89ef',
+                backgroundColor: cfg.favicon.windows,
                 onConflict: 'override',
                 assets: {
                     windows80Ie10Tile: false,
@@ -258,8 +251,8 @@ gulp.task('generate-favicon', function(done) {
             androidChrome: {
                 pictureAspect: 'backgroundAndMargin',
                 margin: '17%',
-                backgroundColor: '#ffffff',
-                themeColor: '#ffffff',
+                backgroundColor: cfg.favicon.androidBackground,
+                themeColor: cfg.favicon.androidColor,
                 manifest: {
                     name: pkg.name,
                     display: 'standalone',
@@ -274,7 +267,7 @@ gulp.task('generate-favicon', function(done) {
             },
             safariPinnedTab: {
                 pictureAspect: 'silhouette',
-                themeColor: '#5bbad5'
+                themeColor: cfg.favicon.safariColor
             }
         },
         settings: {
@@ -284,7 +277,7 @@ gulp.task('generate-favicon', function(done) {
         },
         versioning: {
             paramName: 'v',
-            paramValue: 'yyxQeA5p49'
+            paramValue: cfg.favicon.revisionKey
         },
         markupFile: FAVICON_DATA_FILE
     }, function() {
@@ -296,9 +289,9 @@ gulp.task('generate-favicon', function(done) {
 // this task whenever you modify a page. You can keep this task
 // as is or refactor your existing HTML pipeline.
 gulp.task('inject-favicon-markups', function() {
-    gulp.src([ cfg.paths.app + '_includes/base/head.html' ])
-        .pipe($.realFavicon.injectFaviconMarkups(JSON.parse(fs.readFileSync(FAVICON_DATA_FILE)).favicon.html_code))
-        .pipe(gulp.dest(basePaths.app + '_includes/base/'));
+    gulp.src([ cfg.paths.app + cfg.favicon.html ])
+        .pipe($.realFavicon.injectFaviconMarkups(JSON.parse(fs.readFileSync(cfg.favicon.dataFile)).favicon.html_code))
+        .pipe(gulp.dest(cfg.paths.app + cfg.favicon.htmlDist));
 });
 
 // Check for updates on RealFaviconGenerator (think: Apple has just
@@ -306,7 +299,7 @@ gulp.task('inject-favicon-markups', function() {
 // Run this task from time to time. Ideally, make it part of your
 // continuous integration system.
 gulp.task('check-for-favicon-update', function(done) {
-    var currentVersion = JSON.parse(fs.readFileSync(FAVICON_DATA_FILE)).version;
+    var currentVersion = JSON.parse(fs.readFileSync(cfg.favicon.dataFile)).version;
     $.realFavicon.checkForUpdates(currentVersion, function(err) {
         if (err) {
             throw err;
