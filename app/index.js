4acafe3c7cabc178c331ddd7402c9c00c07bc626
Christoph Böhner-Figas <boehner@team23.de>
2017-10-13 18:20:03 +0200

    Update index to use base class

--- app/index.js
+++ app/index.js
@@ -3,6 +3,88 @@ var util = require('util');
 var path = require('path');
 var yeoman = require('yeoman-generator');
 
+'use strict';
+const Generator = require('yeoman-generator');
+const commandExists = require('command-exists').sync;
+const yosay = require('yosay');
+const chalk = require('chalk');
+const mkdirp = require('mkdirp');
+const _s = require('underscore.string');
+
+
+module.exports = class extends Generator {
+  constructor(args, opts) {
+    super(args, opts);
+
+    this.option('skip-welcome-message', {
+      desc: 'Skips the welcome message',
+      type: Boolean
+    });
+
+    this.option('test-framework', {
+      desc: 'Test framework to be invoked',
+      type: String,
+      defaults: 'mocha'
+    });
+
+    this.option('babel', {
+      desc: 'Use Babel',
+      type: Boolean,
+      defaults: true
+    });
+  }
+
+  initializing() {
+    this.pkg = require('../package.json');
+    this.composeWith(
+      require.resolve(`generator-${this.options['test-framework']}/generators/app`),
+      { 'skip-install': this.options['skip-install'] }
+    );
+  }
+
+  prompting() {
+    if (!this.options['skip-welcome-message']) {
+      this.log(yosay('\'Allo \'allo! I will build a theme resource directory now. Out of the box I include a gulpfile to build your app.'));
+    }
+    return this.prompt([{
+      type    : 'input',
+      name    : 'name',
+      message : 'Your project name',
+      default : this.appname // Default to current folder name
+    }
+  }
+
+  writing() {
+    this._writingGulpfile();
+   // this._writingPackageJSON();
+   // this._writingBabel();
+   // this._writingGit();
+   // this._writingBower();
+   // this._writingEditorConfig();
+   // this._writingH5bp();
+   // this._writingStyles();
+   // this._writingScripts();
+   // this._writingHtml();
+   // this._writingMisc();
+  }
+
+  _writingGulpfile() {
+    this.fs.copyTpl(
+      this.templatePath('gulpfile.js'),
+      this.destinationPath('gulpfile.babel.js'),
+      {
+        date: (new Date).toISOString().split('T')[0],
+        name: this.pkg.name,
+        version: this.pkg.version,
+        includeBabel: this.options['babel'],
+        testFramework: this.options['test-framework']
+      }
+    );
+  }
+
+};
+
+
 var DiazothemeGenerator = module.exports = function DiazothemeGenerator(args, options, config) {
   yeoman.generators.Base.apply(this, arguments);
 
